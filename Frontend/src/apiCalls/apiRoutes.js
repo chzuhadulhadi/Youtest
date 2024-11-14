13623afe72296070a809a20531d24b077bd470df
aliumair90 <aliumair.cs90@gmail.com>
2024-11-15 03:01:24 +0500

    http changed

--- Frontend/src/apiCalls/apiRoutes.js
+++ Frontend/src/apiCalls/apiRoutes.js
@@ -6,10 +6,15 @@ export const environment = "C://Users/user/Downloads/";
 // export const serverImageUrl = "https://testfactory.online/api";
 
 // export const frontEndPath = "https://test-fact.vercel.app/"
-export const frontEndPath = "http://localhost:3000/";
-export const local = "http://localhost:4000/";
-export const serverUrl = "http://localhost:4000/";
-export const serverImageUrl = "http://localhost:4000";
+// export const frontEndPath = "http://localhost:3000/";
+// export const local = "http://localhost:4000/";
+// export const serverUrl = "http://localhost:4000/";
+// export const serverImageUrl = "http://localhost:4000";
+export const frontEndPath = "http:// 3.83.243.222/";
+export const local = "http://3.83.243.222:4000/";
+export const serverUrl = "http://3.83.243.222:4000/";
+export const serverImageUrl = "http://3.83.243.222:4000";
+
 export const signup = local + "api/user/signUp";
 export const forgotpass = local + "api/user/forgotpassword";
 export const resetpass = local + "api/user/resetpassword";
