0257a67b54c540df0f1b8c93afc06cba5ccfc884
chzuhad <chzuhadulhadi@gmail.com>
2024-11-18 21:59:45 +0500

    all common

--- Backend/model/testModel.js
+++ Backend/model/testModel.js
@@ -23,7 +23,7 @@ module.exports = {
 		// return getobj;
 		const qobj = await testService.makeEditTestObj(getobj.dataValues);
 		// console.log(qobj);
-		return { obj, ...qobj, getobj: qobj }
+		return { obj, ...qobj, ...getobj }
 		// return { rows }
 	},
 	getMyTest: async function (obj) {
